03ce128e940b6462ee0df4a2eb01d3d60697c158
PiX <69745008+pixincreate@users.noreply.github.com>
2024-08-06 00:24:28 +0530

    refactor(verifier): verify existence of tools based on flags

--- src/verifier.sh
+++ src/verifier.sh
@@ -11,13 +11,28 @@ verify_downloads() {
   echo "Verifying ${tool}..."
   local tools=("avbroot" "afsr" "my-avbroot-setup" "custota" "msd" "bcr" "oemunlockonboot")
   for tool in "${tools[@]}"; do
-    if [ ! -e "${WORKDIR}/${tool}" ]; then
-      echo "Error: ${tool} not found in ${WORKDIR}"
-      exit 1
+    local tool_upper_case=$(echo "${tool}" | tr '[:lower:]' '[:upper:]')
+
+    if [[ "${tool}" == "my-avbroot-setup" ]]; then
+      FLAG="${ADDITIONALS[MY_AVBROOT_SETUP]}"
+    else
+      FLAG="${ADDITIONALS[$tool_upper_case]}"
+    fi
+
+    if [[ "${FLAG}" == 'true' ]]; then
+      if [[ ! -d "${WORKDIR}/${tool}" ]]; then
+        tool="${tool}.zip"
+      fi
+      if [[ ! -e "${WORKDIR}/${tool}" ]]; then
+        echo "Error: ${tool} not found in ${WORKDIR}"
+        exit 1
+      fi
     fi
   done
-  if [ "${ADDITIONALS[root]}" ]; then
-    if [ ! -e "${WORKDIR}/magisk" ]; then
+
+  if [ "${ADDITIONALS[ROOT]}" == 'true' ]; then
+    echo "Verifying Magisk..."
+    if [ ! -e "${WORKDIR}/magisk.apk" ]; then
       echo "Error: Magisk not found in ${WORKDIR}"
       exit 1
     fi
