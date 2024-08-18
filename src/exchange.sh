43aa7616e711dc4918ac0257908b3c6b035cae2a
PiX <69745008+pixincreate@users.noreply.github.com>
2024-08-19 01:13:33 +0530

    fix: decoder

--- src/exchange.sh
+++ src/exchange.sh
@@ -15,15 +15,33 @@ function base64_encode() {
 function base64_decode() {
   echo -e "Decoding keys from base64..."
 
-  if [[ -n "${KEYS[AVB_BASE64]}" ]]; then
-    echo "${KEYS[AVB_BASE64]}" | base64 --decode > ".tmp/${KEYS[AVB]}"
-  fi
-  if [[ -n "${KEYS[CERT_OTA_BASE64]}" ]]; then
-    echo "${KEYS[CERT_OTA_BASE64]}" | base64 --decode > ".tmp/${KEYS[CERT_OTA]}"
-  fi
-  if [[ -n "${KEYS[OTA_BASE64]}" ]]; then
-    echo "${KEYS[OTA_BASE64]}" | base64 --decode > ".tmp/${KEYS[OTA]}"
-  fi
+  KEYS[AVB_BASE64]="${KEYS_AVB_BASE64:-${KEYS[AVB_BASE64]}}"
+  KEYS[CERT_OTA_BASE64]="${KEYS_CERT_OTA_BASE64:-${KEYS[CERT_OTA_BASE64]}}"
+  KEYS[OTA_BASE64]="${KEYS_OTA_BASE64:-${KEYS[OTA_BASE64]}}"
+
+  local keys=("AVB_BASE64" "CERT_OTA_BASE64" "OTA_BASE64")
+  for key in "${keys[@]}"; do
+    local base64_key="${KEYS[$key]}"
+
+    if [[ -n "${base64_key}" ]]; then
+      base64_key=$(echo "${base64_key}" | tr -d '\n' | tr -d ' ')
+
+      # Generate output file name based on key name
+      local output_file="${KEYS[$(echo ${key} | sed 's/_BASE64$//')]}"
+
+      # Decode bas64 and write to a file
+      echo "${base64_key}" | base64 --decode > "${output_file}" 2> /dev/null
+
+      if [[ $? -ne 0 ]]; then
+        echo "Error decoding base64 for ${key}"
+      else
+        echo "Successfully decoded ${key} to ${output_file}"
+        KEYS[$(echo ${key} | sed 's/_BASE64$//')]="${output_file}"
+      fi
+    else
+      echo "No base64 data found for ${key}"
+    fi
+  done
 
   echo -e "Decoded keys from base64."
 }
