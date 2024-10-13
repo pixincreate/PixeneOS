cf82239ce972f9db7eda2e6abc4c278f4cb38fed
PiX <69745008+pixincreate@users.noreply.github.com>
2024-10-13 22:24:29 +0530

    refactor: make `check_toml_env` as generic as possible

--- src/util_functions.sh
+++ src/util_functions.sh
@@ -435,22 +435,28 @@ function generate_ota_info() {
 }
 
 function check_toml_env() {
+  declare -A config_vars
   toml_file="env.toml"
+
   if [ -f "$toml_file" ]; then
+    while IFS='=' read -r key value; do
+      key=$(echo "$key" | xargs)                                  # Trim whitespace
+      value=$(echo "$value" | xargs | sed -E 's/^"([^"]*)"$/\1/') # Trim whitespace and quotes
+      if [[ -n "$key" && -n "$value" ]]; then
+        config_vars["$key"]="$value"
+      fi
+    done < <(grep -v '^#' "$toml_file") # Ignore comments
 
-    # Read the toml file and extract the required variables
-    DEVICE_NAME=$(grep '^device_name =' "$toml_file" | sed -E 's/^device_name *= *"([^"]*)"/\1/') && success_status=true
-    GRAPHENEOS[UPDATE_CHANNEL]=$(grep '^update_channel =' "$toml_file" | sed -E 's/^update_channel *= *"([^"]*)"/\1/') && success_status=true
-
-    if [[ "${success_status}" == true ]]; then
-      echo -e "Found variables in \`${toml_file}\` and will take precedence over other values. \n"
+    if [[ ${#config_vars[@]} -gt 0 ]]; then
+      echo -e "Found variables in \`${toml_file}\` and will take precedence over other values.\n"
+      for key in "${!config_vars[@]}"; do
+        echo -e "${key}: ${config_vars[$key]}"
+        eval "${key}=${config_vars[$key]}"
+      done
     else
       echo -e "Failed to find the required variables in \`${toml_file}\`.\n"
       exit 1
     fi
-
-    echo -e "DEVICE_NAME: ${DEVICE_NAME}"
-    echo -e "GRAPHENEOS_UPDATE_CHANNEL: ${GRAPHENEOS[UPDATE_CHANNEL]}"
   fi
 }
 
